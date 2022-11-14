393c5040d910123a7d2629807af2b982047b8e94
dsammanthan <92326520+dsammanthan@users.noreply.github.com>
2022-11-14 21:58:32 +0000

    Add files via upload
    This MATLAB code computes the force of the McKibben or pleated PAM as shown in the dissertation.

---
+++ Force_of_McKibben_or_Pleated_PAM.m
@@ -0,0 +1,57 @@
+%% Force of McKibben and Pleated PAM
+%%
+% INPUTTING PARAMETERS
+%Current values show dimensions of final PAM design
+
+%Set L_1, the length of deflated PAM (m)
+L_1=0.04;
+%L_1 = input("Enter the length of deflated PAM (metres)");
+%Set D_E, the diameter of end of PAM (m)
+D_E=0.016;
+%D_E = input("Enter the diameter of end of deflated PAM (metres)");
+
+%%
+%CALCULATING FORCE
+
+%Set up range of lengths of contracting PAM 
+%(factor=0.9 means PAM is 90% of original length, contraction is 10% of original length)
+factor=0.5:0.01:0.99;
+L_2=L_1*factor;
+%For plot later
+factorpercent=(factor.*100);
+
+%Set up the equations needed to find PAM's Force
+%Calculate half length of  PAM
+l_1=L_1./2;
+l_2=L_2./2;
+%Calculate volume of deflated PAM (cylinder)
+V_1= pi*(D_E/2)^2*L_1;
+%Calculate diameter of inflated PAM at widest point
+D_2=D_E+(2*sqrt(l_1.^2 - l_2.^2));
+%Calculate volume of inflated PAM (truncated cones)
+V_2=2*((1/3).*pi.*l_2.*((D_E/2).*(D_2/2) +(D_E/2).^2 +(D_2/2).^2));
+%Calculate difference between deflated and inflated volume
+dV=V_2-V_1;
+%Calculate difference between deflated and inflated length
+dL=L_1-L_2;
+
+%%
+%PLOTTING GRAPH
+
+%Calculate force PAM generates
+for p=[5000,10000,40000]
+%for p=[10000,50000,100000]
+%for p = 10000:10000:100000
+F=2/3*p.*dV./dL;
+%Plot force against contraction
+plot(factorpercent, F)
+hold on
+end
+
+xlabel('Contracted Length (% of Original Length)')
+ylabel('Force (N)')
+title('Force Exerted by McKibben or Pleated PAM as it Contracts')
+set(gca, 'XDir','reverse')
+legend('0.05bar','0.1bar','0.4bar')
+%legend('10kPa','50kPA','100kPA')
+%legend ('1 bar','2 bar','3 bar')
